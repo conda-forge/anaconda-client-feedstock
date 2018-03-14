071bfb6629590d7f4c129b07ef818fead47db3f0
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-03-14 17:01:28 -0400

    Install locally built anaconda-client
    After building `anaconda-client`, install our newly built copy of
    `anaconda-client`. Make sure to do this right after building and before
    uploading. That way we will attempt to upload our new package of
    `anaconda-client` using our new build of `anaconda-client`. IOW this is
    a great test of whether `anaconda-client` is defective. If it turns out
    `anaconda-client` cannot upload itself with our upload script, it will
    cause CIs to error before the upload to occur. Thus avoiding publishing
    copies of `anaconda-client` that would break other feedstocks. We even
    get some of these testing benefits before merging (though obviously not
    the actual uploading part).

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -58,6 +58,7 @@ conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
 conda build /recipe_root --quiet || exit 1
+conda install --yes --quiet --offline --use-local anaconda-client || exit 1
 upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
 touch /feedstock_root/build_artefacts/conda-forge-build-done
